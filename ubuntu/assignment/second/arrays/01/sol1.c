540a90eb5688567f195152554d16c17e4627b680
islam-machine <76726548+islami00@users.noreply.github.com>
2021-12-02 18:03:19 +0100

    Done with arrays and added more desc to zipfmt

--- ubuntu/assignment/second/arrays/01/sol1.c
+++ ubuntu/assignment/second/arrays/01/sol1.c
@@ -2,9 +2,9 @@
 void prettyPrint(int* scores);
 int main() {
   int scores[5] = {67, 74, 56, 90, 87};
-  while (scores[2] != 80) {
+  while (scores[3] != 80) {
     printf("Enter 80: ");
-    scanf("%d", &scores[2]);
+    scanf("%d", &scores[3]);
   }
   while (scores[4] != 76) {
     printf("Enter 76: ");
