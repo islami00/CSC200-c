b1b9463fbb140a99d307ac517a922510eecdb1c8
Islam <islamkwnd@gmail.com>
2022-02-08 18:38:24 +0100

    Defeat of the std input buffer.

--- ubuntu/practice/l5/struct-ex.c
+++ ubuntu/practice/l5/struct-ex.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
+void simpleBufferClear();
 int main() {
   struct book {
     char name[90];
@@ -12,16 +12,13 @@ int main() {
   struct book arr2[2];
   for (size_t i = 0; i != 2; i++) {
     printf("Enter name");
-    // This slurps up the stray \n. COuld be more general. E.g if a float is
-    // passed to pages, it truncates At the "." leaving that and the decimal
-    // part for gets
-    scanf("\n");
     gets(arr2[i].name);
     printf("Enter price");
 
     scanf("%f", &arr2[i].price);
     printf("Enter pages");
     scanf("%d", &arr2[i].pages);
+    simpleBufferClear();
   }
   for (size_t i = 0; i != 2; i++) {
     printf("name %s\n", arr2[i].name);
@@ -29,4 +26,16 @@ int main() {
     printf("pages %d\n", arr2[i].pages);
   }
   return 0;
+}
+// Finally solved the problem. Instead of the scanf hack,
+// use this to read an arbitrary character from stdin.
+// This runs until it reaches \n so it also covers \r\n of windows!.
+// And any other stray bits scanf didn't pick up before last \n Will move to
+// util file when I figure out modules.
+// It's recursive too, haha.
+// Thanks: https://www.geeksforgeeks.org/clearing-the-input-buffer-in-cc/
+void simpleBufferClear() {
+  // Keep clearing till you reach \n
+  char res = getchar();
+  if (res != '\n') simpleBufferClear();
 }
